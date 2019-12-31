2355c5e135f861cb2842bc429342d8e89d281851
seokjoon <seokjoon@gmail.com>
2019-12-31 15:04:38 +0900

    app1

--- djangoTestTodo/settings.py
+++ djangoTestTodo/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # ####### BEGIN
+    'app1',
+    # ####### END
 ]
 
 MIDDLEWARE = [
